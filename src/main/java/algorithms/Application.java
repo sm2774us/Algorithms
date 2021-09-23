fb08d56f1a0a436871def3f82176524bf5bbd83a
sm277 <sm2774us@gmail.com>
2021-09-23 01:02:45 -0400

    First Import

---
+++ src/main/java/algorithms/Application.java
@@ -0,0 +1,13 @@
+package algorithms;
+
+import io.micronaut.runtime.Micronaut;
+
+/**
+ * @author smajumdar
+ */
+public class Application {
+
+  public static void main(String[] args) {
+    Micronaut.run(Application.class, args);
+  }
+}
