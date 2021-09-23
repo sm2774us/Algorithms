21fbe5c60c743a5d62ee8c70f23114571d433d98
sm277 <sm2774us@gmail.com>
2021-09-23 01:02:37 -0400

    First Import

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,10 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    id("com.github.johnrengelman.shadow") version "6.1.0"
     id("io.micronaut.application")
     id("org.jlleitschuh.gradle.ktlint") version "8.1.0"
+	id("com.adarshr.test-logger") version "3.0.0"
     kotlin("jvm") version "1.5.31"
 	kotlin("kapt") version "1.5.31"
 
@@ -44,6 +46,10 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+}
+
+application {
+    mainClass.set("algorithms.Application")
 }
 
 tasks.withType<Test> {
